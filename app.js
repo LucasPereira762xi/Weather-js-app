303f20da004195d1ef2c6e97726aeb7c0fe8e47a
pereira762xi@gmail.com <pereira762xi@gmail.com>
2019-07-06 23:45:56 -0300

    Coletando infos da api e exibindo a temperatura

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
 window.addEventListener('load', ()=>{
     let long;
     let lat;
+    let temperatureDescription = document.querySelector('.description');
+    let temperatureDegree = document.querySelector('.degree');
+    let timezone = document.querySelector('.location-timezone');
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position =>{
             long = position.coords.longitude;
             lat  = position.coords.latitude;
             
-            const api = 'https://api.darksky.net/forecast/79c0804681efa552dfd5807732d660d9/${lat},${long}';
+            // ESTA VARIAVEL RECEBE UM LINK PARA PODER CHAMAR A API DE UM SERVIDOR LOCAL
+            const proxy = 'https://cors-anywhere.herokuapp.com/';
+            // ESTA VARIAVEL CHAMA A API COM AS INFORMAÇÕES DO NAVEGADOR
+            const api = proxy+'https://api.darksky.net/forecast/79c0804681efa552dfd5807732d660d9/'+lat+','+long;
+
+            fetch(api)
+            .then(response =>{
+                return response.json();
+            })
+            .then(data =>{
+                console.log(data);
+                // PEGANDO ELEMENTOS DO DATA
+                // O FORMATO 'const {temperature}' É FORMA AGIL DE COLETAR OS ELEMENTOS
+                const {temperature, summary} = data.currently;
+                // SET DOM ELEMENTS DIRETO DA API
+                const $cel = ((temperature - 32) * 5/9);
+                temperatureDegree.textContent = $cel.toFixed(0);
+            })
         });
 
-        fetch(api)
-            .then(data =>{
-                
-            });
+        
     }else{
         h1.textContent = "SEM LOCALIZAÇÃO :(";
     }
